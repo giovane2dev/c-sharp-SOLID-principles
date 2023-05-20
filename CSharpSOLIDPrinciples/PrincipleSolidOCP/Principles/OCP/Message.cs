6506aaec6fe092863e3cf723e6422eaddcbc7513
Two Dev <contato.twodev@gmail.com>
2023-05-20 20:42:27 -0300

    feat: Applies The Open-Closed Principle
    - Creates Message class and derived class (FeedsMessage) to extend

---
+++ CSharpSOLIDPrinciples/PrincipleSolidOCP/Principles/OCP/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Principles.OCP
+{
+    public class Message
+    {
+        public Message()
+        {
+            
+        }
+
+        public virtual void Create(string shortMessage)
+        {
+            Console.WriteLine(shortMessage);
+        }
+    }
+}
---
+++ CSharpSOLIDPrinciples/PrincipleSolidOCP/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SOLID.Principles.OCP;
+
+public class Program
+{
+public static void Main(String[] args)
+    {
+        // Open-Closed Principle
+
+        // Classes must be open for extension and closed for changes
+
+        var baseClass = new Message();
+
+        var derivedClass = new FeedsMessage(String.Empty);
+
+        // baseClass.Create("Base class: Message");
+
+        string longMessage = """
+            This is a long message.
+            It has several lines.
+                Some are indented
+                        more than others.
+            Some should start at the first column.
+            Some have "quoted text" in them.
+            """;
+
+        derivedClass.Create(longMessage);
+    }
+}
