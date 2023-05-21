001269d63e980ad1adf88a10ea8fa37ee5d1206c
Two Dev <contato.twodev@gmail.com>
2023-05-20 21:49:55 -0300

    feat: Creates New Classes To Apply Inheritance Concepts
    - Creates and replaced method using "virtual" and "override"

---
+++ CSharpSOLIDPrinciples/Inheritance/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Account
+    {
+        public Account(string text = "Default text")
+        {
+            Console.WriteLine(text);
+        }
+
+        private int number;
+        private double balance;
+
+        // use lambda operators: =>
+        // protect property accessor: private set
+        public int Number { 
+            get => this.number;
+            set => this.number = value;
+        }
+        public double Balance { 
+            get => this.balance; 
+            set => this.balance = value; }
+
+        // this balance will replaced, so the why of the "virtual"
+        public virtual void CreateBalance()
+        {
+            Console.WriteLine("This balance is 10!");
+        }
+    }
+}
---
+++ CSharpSOLIDPrinciples/Inheritance/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Inheritance;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        SavingsAccount savingsAccount = new SavingsAccount("New text");
+
+        savingsAccount.CreateBalance();
+    }
+}
