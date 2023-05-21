849e5c286ee930b872a32ad0da1c2d41e185730e
Two Dev <contato.twodev@gmail.com>
2023-05-20 21:58:18 -0300

    chore: Changes For Commit Edit

---
+++ CSharpSOLIDPrinciples/Inheritance/SavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class SavingsAccount : Account
+    {
+        // use "base" to transfer parameter to base class
+        public SavingsAccount(string text) : base(text)
+        {
+            
+        }
+
+        public override void CreateBalance()
+        {
+            Console.WriteLine("This balance is 20!");
+        }
+    }
+}
