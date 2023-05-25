ff343514b4fe5f05fe6b25c5b39cf22814ca46f7
Two Dev <contato.twodev@gmail.com>
2023-05-25 17:17:06 -0300

    refactor: Alters "SOLID" Project And Update Files
    - changes "PrincipleSolidOCP" project for "SOLID"
    - updates files content indexes

---
+++ CSharpSOLIDPrinciples/SOLID/Principles/OCP/FeedsMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SOLID.Principles.OCP
+{
+    public class FeedsMessage : Message
+    {
+        public FeedsMessage(string comment)
+        {
+
+        }    
+
+        public override void Create(string longMessage)
+        {
+            Console.WriteLine(longMessage);
+        }
+    }
+}
