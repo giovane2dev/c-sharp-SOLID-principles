d395a1e15be76e9fd72ade7183487bd46723fb6d
Two Dev <contato.twodev@gmail.com>
2023-05-27 18:32:41 -0300

    feat: Adds New Classes To Inheritance Example
    - Creates Man, Cat And Fish classes for inherit from Animal parent  class

---
+++ CSharpSOLIDPrinciples/Inheritance/CompositionsAndInheritance/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.CompositionsAndInheritance
+{
+    public class Animal
+    {
+        public void ToEat()
+        {
+
+        }
+
+        public void ToWalk()
+        {
+
+        }
+
+        public void ToSleep()
+        {
+
+        }
+    }
+}
