4affb5ef55475a86df4c72616d97647c521652df
Two Dev <contato.twodev@gmail.com>
2023-06-10 14:59:31 -0300

    feat: Includes Dependency Injection Examples
    - creates new classes for demonstration

---
+++ CSharpSOLIDPrinciples/DependencyInjection/Pharmacy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class Pharmacy
+    {
+        private readonly IProduct _iproduct;
+
+        public Pharmacy(IProduct iproduct)
+        {
+            this._iproduct = iproduct;
+        }
+
+        public void AddNewPharmacyProduct(string name)
+        {
+            this._iproduct.AddProduct(name);
+        }
+    }
+}
---
+++ CSharpSOLIDPrinciples/DependencyInjection/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DependencyInjection;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Pharmacy pharmacy = new Pharmacy(new Product());
+
+        pharmacy.AddNewPharmacyProduct("teste");
+    }
+} 
