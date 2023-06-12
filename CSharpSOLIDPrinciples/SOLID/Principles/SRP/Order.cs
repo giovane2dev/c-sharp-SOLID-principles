fa94ce250703950f47f2334447afd366818e7c8c
Two Dev <contato.twodev@gmail.com>
2023-06-12 16:15:13 -0300

    feat: Includes Examples For Single Responsibility Principle - SRP
    - assigns context specific responsibilities

---
+++ CSharpSOLIDPrinciples/SOLID/Principles/SRP/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Principles.SRP
+{
+    internal class Order
+    {
+        private SendMail sendMail;
+        private ILogger ilogger;
+
+        public Order()
+        {
+            sendMail = new SendMail();
+            ilogger = new RegisterLog();
+        }
+
+        public void CreateOrder()
+        {
+            try
+            {
+                sendMail.From = "Giovane";
+                sendMail.To = "Michelle";
+                sendMail.Subject = ".NET";
+                sendMail.Body = "Hello World!";
+                sendMail.Send();
+
+                ilogger.Info("Mail sent");
+            } catch (Exception e)
+            {
+
+            }
+        }
+
+        public void DeleteOrder()
+        {
+
+        }
+    }
+}
---
+++ CSharpSOLIDPrinciples/SOLID/Principles/SRP/RegisterLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Principles.SRP
+{
+    internal class RegisterLog : ILogger
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
