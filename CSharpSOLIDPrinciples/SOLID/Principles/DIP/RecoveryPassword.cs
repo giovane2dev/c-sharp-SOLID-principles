29ba47d6830ce50924266a04884bebc3b7d5f0ac
Two Dev <contato.twodev@gmail.com>
2023-06-13 19:44:28 -0300

    feat: Applies Dependency Inversion Principle
    - adds interface and new concrete low level class for depend of the abstraction
    - adds new service class for injects and invert dependency

---
+++ CSharpSOLIDPrinciples/SOLID/Principles/DIP/RecoveryPassword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Principles.DIP
+{
+    public class RecoveryPassword
+    {
+        IDataBaseConnection dataBaseConnection;
+
+        public RecoveryPassword(IDataBaseConnection dataBaseConnection)
+        {
+            this.dataBaseConnection = dataBaseConnection;
+        }
+    }
+}
---
+++ CSharpSOLIDPrinciples/SOLID/Principles/DIP/SQlServerConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Principles.DIP
+{
+    public class SQlServerConnection : IDataBaseConnection
+    {
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
