22b57b850644616cdad16d739f9af76ce0e8b373
Two Dev <contato.twodev@gmail.com>
2023-06-13 19:46:50 -0300

    fix: Updates Project Files
    - changes file content indexes
    - changes bin and obj files

--- CSharpSOLIDPrinciples/SOLID/Program.cs
+++ CSharpSOLIDPrinciples/SOLID/Program.cs
@@ -1,17 +1,20 @@
-﻿namespace SOLID.Principles.OCP;
+﻿using SOLID.Principles.LSP.Violation.Example1;
+using SOLID.Principles.LSP.Violation.Example2;
+
+namespace SOLID.Principles.OCP;
 
 public class Program
 {
     public static void Main(String[] args)
     {
         // Single Responsability Principle
-
+        
+        /*
         // Classes must be open for extension and closed for changes
 
         // var baseClass = new Message();
         // baseClass.Create("Base class: Message");
-
-        /*
+        
         var derivedClass = new FeedsMessage(String.Empty);
 
         string longMessage = """
@@ -28,8 +31,34 @@ public class Program
 
         // Open-Closed Principle
 
+        /*
         FeedsMessage feed = new FeedsMessage();
 
         feed.Create(longMessage: "This is long message!");
+        */
+
+        // Liskov Substitution Principle
+
+        /*
+        Employee employee1 = new Manager("Giovane", "Cargo");
+        Employee employee2 = new Seller("Pessoa", "Cargo");
+
+        // Console.WriteLine(employee1.GetType());
+        // Console.WriteLine(employee2.GetType());
+
+        var salary = employee1.CalculateSalary(10000);
+
+        Console.WriteLine(salary);
+        
+        var numbers = new int[] { 10, 15, 20, 25};
+
+        Calculate calculateSum1 = new CalculateSum(numbers);
+        Calculate calculateSum2 = new CalculateSumEvenNumbers(numbers);
+
+        Console.WriteLine($"The sum of numbers is {calculateSum1.Calc()}!");
+        Console.WriteLine($"The sum of even numbers is {calculateSum2.Calc()}!");
+        */
+
+        // Interface Segregation Principle
     }
 }
