c408f0ef5e2f6a7f2e43640fe237df9e78127b95
Two Dev <contato.twodev@gmail.com>
2023-06-13 18:19:39 -0300

    feat: Applies Interface Segregation Principle
    - adds new interfaces and concrete class for segregation

---
+++ CSharpSOLIDPrinciples/SOLID/Principles/ISP/IMailMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SOLID
+{
+    public interface IMailMessage
+    {
+        public void SendMail();
+    }
+}
---
+++ CSharpSOLIDPrinciples/SOLID/Principles/ISP/IPersistence.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SOLID
+{
+    public interface IPersistence
+    {
+        public void ValidateData();
+        public void SaveData();    
+    }
+}
