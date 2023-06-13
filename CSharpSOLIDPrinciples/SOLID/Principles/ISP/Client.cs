63672499adae7f357ae2f4e3f665cd20a03585a1
Two Dev <contato.twodev@gmail.com>
2023-06-13 18:19:39 -0300

    feat: Applies Interface Segregation Principle
    - adds new interfaces and concret class for segregation

---
+++ CSharpSOLIDPrinciples/SOLID/Principles/ISP/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Principles.ISP
+{
+    internal class Client : IPersistence, IMailMessage
+    {
+        public void SaveData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
